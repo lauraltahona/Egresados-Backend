9e53d69a564283cf9971f87f69b57b4b9a5404cb
lauraltahona <lauraaltahona01@gmail.com>
2026-04-22 23:17:47 -0500

    CORS

--- src/main.py
+++ src/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
 from src.controller.usuario_controller.usuario_controller_crud import usuario_router
 from src.controller.rol_controller.rol_controller_crud import rol_router
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Configuración de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],      # Permite todos los orígenes
+    allow_credentials=True,
+    allow_methods=["*"],      # Permite todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],      # Permite todos los headers
+)
+
 
 app = FastAPI()
 
