59206087b948d1622ea738e874791a7770eeb436
Aldemar <aldemarje73@gmail.com>
2026-04-27 19:59:25 -0500

    Papaya?

--- src/model/proyecto.py
+++ src/model/proyecto.py
@@ -1,7 +1,5 @@
 from pydantic import BaseModel, Field
 
-
-
 class Proyecto(BaseModel):
     idProyecto: int # si es auto incremental quitar de todas las tablas
     tituloProyecto: str = Field(min_length=5, max_length=200, pattern=r"^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+$")
