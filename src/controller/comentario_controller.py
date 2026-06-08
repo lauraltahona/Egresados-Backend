bef13aa591ea812e200fe612c1e154fc4b808a81
lauraltahona <lauraaltahona01@gmail.com>
2026-06-08 14:43:50 -0500

    Add content moderation validation and extend Egresado retrieval with graduation date

--- src/controller/comentario_controller.py
+++ src/controller/comentario_controller.py
@@ -1,13 +1,18 @@
 from fastapi import APIRouter
 from src.service.comentario_service import ComentarioPublicacionService
 from src.modelDto.comentario_dto import ComentarioPublicacionDto
+from src.service.moderador_service import ModeracionService
 
 comentario_router = APIRouter(prefix="/comentario", tags=["Comentarios Publicaciones"])
 
 @comentario_router.post("/")
 async def crear_comentario(comentario: ComentarioPublicacionDto):
-    return await ComentarioPublicacionService.crear_comentario(comentario)
+    validacion = await ModeracionService.validar_contenido(comentario.contenidoComentario)
+    response = await ComentarioPublicacionService.crear_comentario(comentario)
+    return response
 
-@comentario_router.post("/responder")
-async def responder_comentario(comentario: ComentarioPublicacionDto):
-    return await ComentarioPublicacionService.responder_comentario(comentario)
+@comentario_router.post("/responder/{idComentarioPadre}")
+async def responder_comentario(idComentarioPadre: int, comentario: ComentarioPublicacionDto):
+    validacion = await ModeracionService.validar_contenido(comentario.contenidoComentario)
+    response = await ComentarioPublicacionService.responder_comentario(idComentarioPadre, comentario)
+    return response
